8adb1fb29ec460aed1512d3212e58edbfbddf79f
ABBY <abbyW@moringa.com>
2022-03-15 11:04:07 +0300

    Make adjustments on file.

--- js/scripts.js
+++ js/scripts.js
@@ -10,8 +10,14 @@ $(document).ready(function() {
         $(".icon-hidden").toggle();
 });
 });
-document.getElementById("submit").addEventListener("click", (Event) => {
-    Event.preventDefault();
-    main();
+$(document).ready(function() {
+    $("form#mc_embed_signup").submit(function(event) {
+    event.preventDefault();
+    var name = $("input#mce-NAME").val();
+    var email = $("input#mce-Email").val();
+    var message = $("input#textarea").val();
+    alert("Message received. Thankyou for reaching out to us.")
+
+    });
 });
 
